b03ff0ad3d9cb2b4e090a8f84fb17d09325fc453
Jenna Smith <jenna@growldesign.co.uk>
2018-06-17 18:44:28 +0100

    Ensure tweets are by unique authors

--- src/shared/TwitterFeedContainer/TwitterFeedContainerQuery.js
+++ src/shared/TwitterFeedContainer/TwitterFeedContainerQuery.js
@@ -8,6 +8,7 @@ const GET_TWEETS = gql`
 		tweets(query: $query, count: $count) {
 			url
 			html
+			author_url
 		}
 	}
 `;
@@ -17,9 +18,19 @@ export const TwitterFeedContainerQuery = ({ render }) => {
 	Musicoin AND Platform -filter:retweets`;
 
 	return (
-		<Query query={GET_TWEETS} variables={{ query, count: 3 }}>
-			{({ loading, error, data }) =>
-				error || loading ? null : render(data.tweets)}
+		<Query query={GET_TWEETS} variables={{ query, count: 20 }}>
+			{({ loading, error, data }) => {
+				if (error || loading) {
+					return null;
+				}
+
+				const authors = data.tweets.map(tweet => tweet.author_url);
+				const uniqueAuthorTweets = data.tweets.filter((tweet, index) => {
+					return authors.indexOf(tweet.author_url) === index;
+				});
+
+				return render(uniqueAuthorTweets.slice(0, 3));
+			}}
 		</Query>
 	);
 };
