564b8c4e4573c15770db7a89e25c2bd04f943227
bengyles <bengyles@users.noreply.github.com>
2019-07-02 18:06:20 +0200

    Add stats to homepage and change exchange links (#25)
    * Rough version for live stats
    
    * #24 removed Cryptopia from exchanges and faq, added Dove Wallet instead
    
    * Added stats for homepage

--- src/Home/Stats/index.js
+++ src/Home/Stats/index.js
@@ -1,36 +1,77 @@
-import React, { Fragment } from 'react';
-import { PrettyText } from '@/shared/PrettyText';
-import { Numeral } from '@/shared/Numeral';
-import { Wrapper, Item, Label } from './styles';
-import { StatsQuery } from './StatsQuery';
+import React from 'react';
+import AnimatedNumber from 'react-animated-number';
 
-export const Stats = () => {
-	return (
-		<Wrapper>
-			<StatsQuery
-				render={({ totalArtists, totalReleases, totalPlays, priceUsd }) => (
-					<Fragment>
-						<Item>
-							<PrettyText>
-								<Numeral>{totalArtists}</Numeral>
-							</PrettyText>
-							<Label>Verified Musicians</Label>
-						</Item>
-						<Item>
-							<PrettyText>
-								<Numeral>{totalReleases}</Numeral>
-							</PrettyText>
-							<Label>Verified Tracks</Label>
-						</Item>
-						<Item>
-							<PrettyText>
-								<Numeral>{totalPlays}</Numeral>
-							</PrettyText>
-							<Label>Total Plays</Label>
-						</Item>
-					</Fragment>
-				)}
-			/>
-		</Wrapper>
-	);
+let numberStyle = {
+	transition: '0.8s ease-out',
+	transitionProperty: 'background-color, color, opacity',
 };
+
+class Stats extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			totalArtistsTipped: '0',
+			totalTracksTipped: '0',
+			totalPlayed: '0',
+		};
+	}
+
+	render() {
+		return (
+			<div style={{ marginTop: 40 }}>
+				<h2 style={{ textAlign: 'center', fontWeight: 'medium' }}>
+					We have served {this.numberComponent(this.state.totalPlayed)} streams,
+					paid {this.numberComponent(this.state.totalArtistsTipped)}(tips) +{' '}
+					{this.numberComponent(this.state.totalTracksTipped)}(PPP)
+					<span style={{ color: '#F3921B' }}> $MUSIC</span> to musicians.
+				</h2>
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(() => this.getStats(), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	numberComponent(number) {
+		return (
+			<AnimatedNumber
+				component="text"
+				value={number}
+				style={numberStyle}
+				duration={300}
+				formatValue={n => this.numberWithCommas(n)}
+				stepPrecision={0}
+			/>
+		);
+	}
+
+	getStats() {
+		fetch(
+			`${process.env.MUSICOIN_API_ENDPOINT}/manage/analytics?password=${process
+				.env.ADMIN_PWD}`
+		)
+			.then(response => response.json())
+			.then(data =>
+				this.setState({
+					totalTracksTipped: data.releaseTips,
+					totalArtistsTipped: data.userTips,
+					totalPlayed: data.playCount,
+				})
+			);
+	}
+
+	numberWithCommas(number) {
+		if (number == 0) {
+			return '0,000,000';
+		} else {
+			return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+		}
+	}
+}
+
+export { Stats };
